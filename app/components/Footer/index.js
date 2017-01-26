9df8825c24abf7345dcc6213a3d5896bba4d8e7a
Tania Manoli <github@atypicalrobot.com>
2017-01-26 21:52:42 +0000

    Fixes to footer and fixes to Image Zoom plugin

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -10,18 +10,12 @@ function Footer() {
   return (
     <Wrapper>
       <section>
-        <FormattedMessage {...messages.licenseMessage} />
+        <FormattedMessage {...messages.authorMessage} />
+      </section>
+      <section>
       </section>
       <section>
         <LocaleToggle />
-      </section>
-      <section>
-        <FormattedMessage
-          {...messages.authorMessage}
-          values={{
-            author: <A href="https://twitter.com/mxstbr">Max Stoiber</A>,
-          }}
-        />
       </section>
     </Wrapper>
   );
--- app/components/PoiMarker/index.js
+++ app/components/PoiMarker/index.js
@@ -24,7 +24,7 @@ function PoiMarker(props) {
   if (props.poi.plugin.slug == 'video') {
 	  poiInnerContent = (<ReactPlayer url={props.poi.metadata.video_url} width='600' height='350' playing />);
   } if (props.poi.plugin.slug == 'imagezoom') {
-	  poiInnerContent = (<ImageZoom image={{ src: props.poi.metadata.image, className: 'img', style: { width: '25em' } }} zoomImage={{ src: props.poi.metadata.zoomImage, }} />);
+	  poiInnerContent = (<ImageZoom image={{ src: props.poi.metadata.image, className: 'img', style: { width: '25em' } }} zoomImage={{ src: props.poi.metadata.zoom_image, }} />);
   } if (props.poi.plugin == 'timeline') {
     //const timelineItems = JSON.parse(props.poi.metadata.timelineItems)
     //poiInnerContent = (<Timeline options={timelineOptions} items={timelineItems} />);
